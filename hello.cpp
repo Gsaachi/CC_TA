e891463bffbc55bece567b5bb7325742a6f7194a
SG <136472564+Gsaachi@users.noreply.github.com>
2025-03-11 13:48:57 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES1UG22AM139-1\n";
+  cout << "CCLAB_PES1UG22AM139\n";
+  cout << "Im in sem6  \n";
+  return 0;
+}
